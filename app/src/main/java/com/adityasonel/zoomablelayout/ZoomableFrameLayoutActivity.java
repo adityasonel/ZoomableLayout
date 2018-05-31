2f05d937d91502b2bfed5a9157cba3bd27213789
Aditya Sonel <aditya.sonel@outlook.com>
2018-05-31 11:43:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/adityasonel/zoomablelayout/ZoomableFrameLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.adityasonel.zoomablelayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ZoomableFrameLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoomable_frame_layout);
+    }
+}
